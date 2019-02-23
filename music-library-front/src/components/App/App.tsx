6140ead13de29fc6957bb0971a70f73d4a2da0d3
ArcusDeri <michniak@yandex.com>
2019-02-23 09:08:42 +0100

    Add react-router-dom and create simple routing.

---
+++ music-library-front/src/components/App/App.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from '../Home/Home';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
